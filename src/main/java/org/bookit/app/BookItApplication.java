cef69ef582b5468d00d5fc577881f479b093fb0e
Krunal Lukhi <kdlukhi@Krunals-MacBook-Air.local>
2023-08-05 22:07:01 +0530

    indentation and bootjar

--- src/main/java/org/bookit/app/BookItApplication.java
+++ src/main/java/org/bookit/app/BookItApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookItApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookItApplication.class, args);
+    }
 
 }
