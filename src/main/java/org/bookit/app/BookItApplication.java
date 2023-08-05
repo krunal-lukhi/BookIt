e4c8f71397499a450b64ed2c60b039bf46497c0f
Krunal Lukhi <kdlukhi@Krunals-MacBook-Air.local>
2023-08-05 19:07:53 +0530

    Initial commit

---
+++ src/main/java/org/bookit/app/BookItApplication.java
@@ -0,0 +1,13 @@
+package org.bookit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookItApplication.class, args);
+	}
+
+}
